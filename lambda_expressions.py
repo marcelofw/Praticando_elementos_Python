0a66d6ff6109f7d94ae9e9dcfb588317e8684c28
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-04-16 09:34:26 -0300

    another .py file

---
+++ lambda_expressions.py
@@ -0,0 +1,28 @@
+# soma_um = lambda x: x + 1
+# print(soma_um(2))
+
+# multiplica_dois = lambda x: x * 2
+# print(multiplica_dois(3))
+
+# juntar = lambda a, b: a + " " + b
+# print(juntar("Bom", "dia!"))
+
+# maior_teste = lambda x: x > 10
+# print(maior_teste(9))
+
+# mensagem = lambda: "Bom dia!"
+# print(mensagem())
+
+# comeca_com_a = lambda s: s[0].lower() == "a"    #s é o parâmetro e "abacate" é o argumento?
+# print(comeca_com_a("abacate"))
+
+# absoluto = lambda x: x if x >= 0 else -x
+# print(absoluto(2))
+# print(absoluto(-2))
+
+# par_ou_impar = lambda x: "par" if x % 2 == 0 else "ímpar"
+# print(par_ou_impar(2))
+# print(par_ou_impar(3))
+
+maior = lambda a, b: a if a > b else b
+print(maior(10,4))
