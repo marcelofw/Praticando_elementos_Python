7cdb1216a1fa92f6e0df150f75d7bc02f4e0c5bd
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-04-18 09:32:30 -0300

    another .py file

---
+++ enumerate_builtin.py
@@ -0,0 +1,24 @@
+# frutas = ["maçã", "banana", "uva"]
+# for indice, fruta in enumerate(frutas, 1):
+#     print(indice, fruta)
+
+# for indice, fruta in enumerate(frutas, 0):
+#     print(indice, fruta)
+
+# for indice, fruta in enumerate(frutas, start = 1):
+#     print(indice, fruta)
+
+# for indice, fruta in enumerate(frutas):
+#     print(indice, fruta)
+
+# nomes = ["Ana", "Bruno", "Clara"]
+# for i, nome in enumerate(nomes, start = 1):
+#     print(f"{i}. {nome}")
+
+# texto = "Python"
+# for i, letra in enumerate(texto):
+#     print(f"Letra {i}: {letra}")
+
+cores = ["azul", "verde", "amarelo"]
+enumeradas = list(enumerate(cores))
+print(enumeradas)
